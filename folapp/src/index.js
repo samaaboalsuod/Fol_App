0fd5631d2709c211fcf720c7cf033054a5db9a81
samaaboalsuod <samaaaboalsuod@gmail.com>
2026-04-28 15:23:49 +0300

    changes to close

--- folapp/src/index.js
+++ folapp/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import RoutingApp from './RoutingApp';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const startApp = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <RoutingApp />
+    </React.StrictMode>
+  );
+};
 
-    <RoutingApp />
-
-  </React.StrictMode>
-);
+if (window.cordova) {
+  document.addEventListener('deviceready', startApp, false);
+} else {
+  startApp();
+}
 
